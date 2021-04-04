b882690cbea9ae31a6ec5caf6de593aea22ac74b
danderson <danderson@orion.home>
2021-04-04 03:28:40 -0600

    Add Jest

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,3 @@ import { logger } from "./loaders/logger";
 export const greetings = (person: string): string => {
     return `Hello ${person}. How are you?`;
 }
-
-let user = "Jane";
-
-logger.info(greetings(user));
